4f0db56b6198350b775b05fb4f6a4fd470c83597
Anirudh Narasimhan <thenoobdude21.12@gmail.com>
2024-10-08 15:06:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 # step 1: import requests
+import requests
 
 # step 2: call api with given endpoint and key
+response = requests.get("https://api.aviationstack.com/v1/flights?access_key=9b6a85505fed243b921626af0eb662bb")
 
 # step 3: convert to dictionary and grab only the "data" value
+data = response.json()["data"]
 
 # step 4: loop through the flights and check which flights are active
+print("Active Flights:\n-------------------")
+for x in data:
+    if x["flight_status"] == "active":
+        dep = x["departure"]["airport"]
+        arr = x["arrival"]["airport"]
+        name = x["airline"]["name"]
+        print(f"Airline: {name}\nDeparture Airport: {dep}\nArrival Airport: {arr}\n-------------------")
+
 
 # step 5: if active, print the airline, date, departure airport and arrival airport
